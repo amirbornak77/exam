bf9aa393910e9ad064d89a55589ee7b98316ac3c
amirbornak77 <131398939+amirbornak77@users.noreply.github.com>
2023-09-04 10:14:35 -0700

    adding dynamic urls
    such as base.html ,and editing context for better results

--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def cv_view(request):
-    context = {'name':'Amir', 'last_name':'Bornak'}
+    context = {'name':'Amir', 'last_name':'Bornak', 'number': '(123)456-7890',
+    'email': 'anyone@website.com', 'address' : '1600 Amphitheatre Parkway Mountain View, CA 94043 US'       }
     return render(request, 'cv/index.html', context)
